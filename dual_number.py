cacce1570f3c578abeda845853f06693d780d1c8
Mikail Muzakki <mikailmmuzakki@gmail.com>
2023-03-23 16:41:05 +0700

    added __str__ and __repr__ method to dual_number.py

--- dual_number.py
+++ dual_number.py
@@ -2,3 +2,13 @@ class Dual:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+    def __str__(self):
+        if self.b == 1:
+            return f'{self.a}+ε'
+        if self.b >= 0:
+            return f'{self.a}+{self.b}ε'
+        return f'{self.a}{self.b}ε'
+
+    def __repr__(self):
+        return self.__str__()
