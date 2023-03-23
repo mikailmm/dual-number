067fc66113e2ea4612ccf906b02ca67de7cee72a
Mikail Muzakki <mikailmmuzakki@gmail.com>
2023-03-23 16:43:36 +0700

    added dunder operation methods - Addition (__add__ and __radd__) - Subtraction (__sub__ and __rsub__) - Multiplication (__mul__ and __rmul__)

--- dual_number.py
+++ dual_number.py
@@ -1,3 +1,6 @@
+from numbers import Number
+
+
 class Dual:
     def __init__(self, a, b):
         self.a = a
@@ -12,3 +15,46 @@ class Dual:
 
     def __repr__(self):
         return self.__str__()
+
+    def __add__(self, other):
+        if isinstance(other, Dual):
+            return Dual(self.a + other.a, self.b + other.b)
+        elif isinstance(other, Number):
+            return Dual(self.a + other, self.b)
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, Dual):
+            return Dual(self.a - other.a, self.b - other.b)
+        elif isinstance(other, Number):
+            return Dual(self.a - other, self.b)
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        if isinstance(other, Dual):
+            return Dual(other.a - self.a, self.b - other.b)
+        elif isinstance(other, Number):
+            return Dual(other - self.a, -self.b)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Dual):
+            return Dual(self.a*other.a, self.a*other.b + self.b*other.a)
+        elif isinstance(other, Number):
+            return Dual(self.a*other, self.b*other)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        if isinstance(other, Dual):
+            return Dual(self.a*other.a, self.b*other.a + self.a*other.b)
+        elif isinstance(other, Number):
+            return Dual(self.a*other, self.b*other)
+        else:
+            return NotImplemented
